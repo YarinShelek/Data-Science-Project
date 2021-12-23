65e40474896124ed0103f43dfb91a6f3066d03c3
YarinShelek <yarinshelek@gmail.com>
2021-12-23 19:06:32 +0200

    bug fix

--- crawl.py
+++ crawl.py
@@ -12,22 +12,22 @@ try:
     i = -1
 
     while True:
-        i += random.randint(1, 11)
-        players_list = driver.find_elements(By.CLASS_NAME, "summoner")
-        ads = driver.find_elements(By.ID, "desktop-anchor-close")
-        if len(ads)>0: #close ads
+        i += random.randint(1, 11) #increase players search number randomly (1-10)
+        players_list = driver.find_elements(By.CLASS_NAME, "summoner") #get all players in page
+        ads = driver.find_elements(By.ID, "desktop-anchor-close") #find ads
+        if len(ads)>0:
             for k in range(len(ads)):
-                ads[k].click()
-        players_list[i].click() #enter player page
+                ads[k].click() #close ad
+        players_list[i].click() #enter player number i page
         time.sleep(5)
-        players_stats = {}
+        players_stats = {} # stats dict
         #ranking
         rank = driver.find_element(By.CLASS_NAME, "rank-text").find_element(By.TAG_NAME, "strong").text  #get his ranking info
         players_stats["Rank"] = rank
 
         #stats page
-        stats = driver.find_elements(By.CLASS_NAME, "nav-tab-link") #get to stats page
-        stats[1].click()
+        stats = driver.find_elements(By.CLASS_NAME, "nav-tab-link")
+        stats[1].click() #get to stats page
 
         #w/r
         WinRate = driver.find_element(By.CLASS_NAME, "champion-rates") #w/r + games
@@ -41,7 +41,6 @@ try:
 
         #kda
         kda_stats = driver.find_elements(By.CLASS_NAME, "kda")[0].find_elements(By.TAG_NAME, "strong")
-
 
         players_stats["KDA"] = float(kda_stats[0].text)
         players_stats["AVG_kills"] = float(kda_stats[1].text)
@@ -61,7 +60,6 @@ try:
 
         #multi-kill
         players_stats["Multi_Kill"] = 0
-
         for i in range(4):
             if driver.find_elements(By.CLASS_NAME, "multikill-cell")[i+4].text != "—":
                 players_stats["Multi_Kill"] += int(driver.find_elements(By.CLASS_NAME, "multikill-cell")[i+4].text)
@@ -75,8 +73,8 @@ try:
         driver.back()
         time.sleep(5)
 
-        if i >= len(players_list):
-            if p==0:
+        if i >= len(players_list): #check if done with page
+            if p==0: #if first page
                 driver.find_element(By.CLASS_NAME, "skip-to").click() #next page
             if p<=10:
                 p += 1
@@ -89,12 +87,12 @@ try:
                     p += 2000
                 if p >= 20000 and p < 30000:
                     p += 3000
-                if p >= 20000:
-                    p+=10000
+                if p >= 30000:
+                    p+=4500
             driver.get(url+f"&page={p}")
             i = -1
             time.sleep(6)
-        stop = driver.find_elements(By.CLASS_NAME, "content-section leaderboard_table_error")
+        stop = driver.find_elements(By.CLASS_NAME, "content-section leaderboard_table_error") #check for no-content error (means we are overflowing players pages, no content left)
         if len(stop) > 0:
             break
 

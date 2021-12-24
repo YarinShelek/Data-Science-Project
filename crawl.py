0792027fcd7afe5f8d1ae3ddf6e3fc157bf9293a
YarinShelek <yarinshelek@gmail.com>
2021-12-24 10:32:10 +0200

    fixed duplication bug. made code prettier. new parameter names.

--- crawl.py
+++ crawl.py
@@ -7,90 +7,91 @@ try:
     driver = webdriver.Firefox()
     url = "https://u.gg/leaderboards/ranking?region=euw1"
     driver.get(url)
-    time.sleep(5)
-    p = 0
-    i = -1
+    time.sleep(5) #wait for the page to load
+
+    page = 0
+    curr_player = -1
 
     while True:
-        i += random.randint(1, 11) #increase players search number randomly (1-10)
+        curr_player += random.randint(1, 11) #increase players search number randomly (1-10)
         players_list = driver.find_elements(By.CLASS_NAME, "summoner") #get all players in page
         ads = driver.find_elements(By.ID, "desktop-anchor-close") #find ads
         if len(ads) > 0:
-            for k in range(len(ads)):
-                ads[k].click() #close ad
-        if i < len(players_list):
-            players_list[i].click() #enter player number i page
-        time.sleep(5)
-        players_stats = {} # stats dict
-        #ranking
-        rank = driver.find_element(By.CLASS_NAME, "rank-text").find_element(By.TAG_NAME, "strong").text  #get his ranking info
-        players_stats["Rank"] = rank
+            for ad in range(len(ads)):
+                ads[ad].click() #close ad
+        if curr_player < len(players_list):
+            players_list[curr_player].click() #enter player number i page
+            time.sleep(5) #wait for player page to load
+            players_stats = {} # stats dict
+            #ranking
+            rank = driver.find_element(By.CLASS_NAME, "rank-text").find_element(By.TAG_NAME, "strong").text  #get his ranking info
+            players_stats["Rank"] = rank
 
-        #stats page
-        stats = driver.find_elements(By.CLASS_NAME, "nav-tab-link")
-        stats[1].click() #get to stats page
+            #stats page
+            stats = driver.find_elements(By.CLASS_NAME, "nav-tab-link")
+            stats[1].click() #get to stats page
 
-        #w/r
-        WinRate = driver.find_element(By.CLASS_NAME, "champion-rates") #w/r + games
-        win_rate = WinRate.text.split("/")[0].split("%")[0] #w/r
-        win = WinRate.text.split("/")[1].split("W")[0] #wins
-        loss = WinRate.text.split("/")[1].split("W")[1].split("L")[0] #losses
-        games = int(win)+int(loss)
+            #w/r
+            WinRate = driver.find_element(By.CLASS_NAME, "champion-rates") #w/r + games
+            win_rate = WinRate.text.split("/")[0].split("%")[0] #w/r
+            win = WinRate.text.split("/")[1].split("W")[0] #wins
+            loss = WinRate.text.split("/")[1].split("W")[1].split("L")[0] #losses
+            games = int(win)+int(loss)
 
-        players_stats["WinRate"] = int(win_rate)
-        players_stats["Games"] = games
+            players_stats["WinRate"] = int(win_rate)
+            players_stats["Games"] = games
 
-        #kda
-        kda_stats = driver.find_elements(By.CLASS_NAME, "kda")[0].find_elements(By.TAG_NAME, "strong")
+            #kda
+            kda_stats = driver.find_elements(By.CLASS_NAME, "kda")[0].find_elements(By.TAG_NAME, "strong")
 
-        players_stats["KDA"] = float(kda_stats[0].text)
-        players_stats["AVG_kills"] = float(kda_stats[1].text)
-        players_stats["AVG_deaths"] = float(kda_stats[2].text)
-        players_stats["AVG_assists"] = float(kda_stats[3].text)
+            players_stats["KDA"] = float(kda_stats[0].text)
+            players_stats["AVG_kills"] = float(kda_stats[1].text)
+            players_stats["AVG_deaths"] = float(kda_stats[2].text)
+            players_stats["AVG_assists"] = float(kda_stats[3].text)
 
-        #cs
-        players_stats["Creep_Score"] = float(driver.find_elements(By.CLASS_NAME, "cs-cell")[1].text)
+            #cs
+            players_stats["Creep_Score"] = float(driver.find_elements(By.CLASS_NAME, "cs-cell")[1].text)
 
-        #damage
-        dmg = driver.find_elements(By.CLASS_NAME, "damage-cell")[1].text.split(",")
-        players_stats["Damage"] = int(dmg[0] + dmg[1])
+            #damage
+            dmg = driver.find_elements(By.CLASS_NAME, "damage-cell")[1].text.split(",")
+            players_stats["Damage"] = int(dmg[0] + dmg[1])
 
-        #gold
-        gold = driver.find_elements(By.CLASS_NAME, "gold-cell")[1].text.split(",")
-        players_stats["Gold"] = int(gold[0]+gold[1])
+            #gold
+            gold = driver.find_elements(By.CLASS_NAME, "gold-cell")[1].text.split(",")
+            players_stats["Gold"] = int(gold[0]+gold[1])
 
-        #multi-kill
-        players_stats["Multi_Kill"] = 0
-        for i in range(4):
-            if driver.find_elements(By.CLASS_NAME, "multikill-cell")[i+4].text != "—":
-                players_stats["Multi_Kill"] += int(driver.find_elements(By.CLASS_NAME, "multikill-cell")[i+4].text)
+            #multi-kill
+            players_stats["Multi_Kill"] = 0
+            for kill in range(4): #combine the 4 multi kill types into 1 multi-kill
+                if driver.find_elements(By.CLASS_NAME, "multikill-cell")[kill+4].text != "—": #needs to be kill+4
+                    players_stats["Multi_Kill"] += int(driver.find_elements(By.CLASS_NAME, "multikill-cell")[kill+4].text)
 
-        df = pd.DataFrame([players_stats])
-        with open("Data.csv", "a") as file:
-            df.to_csv(file, index=False, header=False)
+            df = pd.DataFrame([players_stats])
+            with open("Data.csv", "a") as file:
+                df.to_csv(file, index=False, header=False)
 
-        driver.back()
-        time.sleep(5)
-        driver.back()
-        time.sleep(5)
+            driver.back()
+            time.sleep(5)
+            driver.back()
+            time.sleep(5)
 
-        if i >= len(players_list): #check if done with page
-            if p==0: #if first page
+        if curr_player >= len(players_list): #check if done with page
+            if page == 0: #if first page
                 driver.find_element(By.CLASS_NAME, "skip-to").click() #next page
-            if p<=10:
-                p += 1
+            if page <= 10:
+                page += 1
             else:
-                if p < 1000:
-                    p += 500
-                if p >= 1000 and p< 10000:
-                    p+=1000
-                if p >= 10000 and p< 20000:
-                    p += 2000
-                if p >= 20000:
-                    p += 3000
+                if page < 1000:
+                    page += 500
+                if 1000 <= page < 10000:
+                    page += 1000
+                if 10000 <= page < 20000:
+                    page += 2000
+                if page >= 20000:
+                    page += 3000
 
-            driver.get(url+f"&page={p}")
-            i = -1
+            driver.get(url+f"&page={page}")
+            curr_player = -1
             time.sleep(6)
         stop = driver.find_elements(By.CLASS_NAME, "content-section leaderboard_table_error") #check for no-content error (means we are overflowing players pages, no content left)
         if len(stop) > 0:
@@ -99,4 +100,4 @@ try:
     driver.close()
 
 except Exception as e:
-    print(f'crashed at {p}')
+    print(f'crashed at page {page}')

a1f5ef5a0df8a3d75a6abc5b380d9300e38e3570
YarinShelek <yarinshelek@gmail.com>
2021-12-28 18:09:24 +0200

    changed to explicit waits as time.sleep() is taking forever (we get 1k data in 4hour...)

--- crawl.py
+++ crawl.py
@@ -4,13 +4,19 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 from datetime import datetime
 import random
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 try:
-    driver = webdriver.Firefox()
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
     url = "https://u.gg/leaderboards/ranking?region=euw1"
+    url = "https://u.gg/leaderboards/ranking?region=euw1&page=2214" #CHANGE TO CHANGE CRAWLING PAGE
+    page = 2214 #CHANGE TO CHANGE CRAWLING PAGE
     driver.get(url)
     time.sleep(10) #wait for the page to load
     data = 0
-    page = 1
     curr_player = -1
 
     while True:
@@ -24,7 +30,8 @@ try:
 
         if curr_player < len(players_list):
             players_list[curr_player].click() #enter player number i page
-            time.sleep(10) #wait for player page to load
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "flex-center"))) #wait for page to load
+            #^^from selenium docs, "how to use explicit wait", wait for element to load... is said to be faster than time.sleep()
             players_stats = {} # stats dict
 
             #check if u.gg is trolling us:
@@ -34,15 +41,19 @@ try:
                     driver.back()
                     time.sleep(10)
                     continue
-
+            driver.find_element(By.CLASS_NAME, "flex-center").click() #update the rank
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "rank-text"))) #wait for update to finish
             #ranking
             rank = driver.find_element(By.CLASS_NAME, "rank-text").find_element(By.TAG_NAME, "strong").text  #get his ranking info
             players_stats["Rank"] = rank
-
             #stats page
             stats = driver.find_elements(By.CLASS_NAME, "nav-tab-link")
             stats[1].click() #get to stats page
-
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "champion-rates"))) #wait for page to load
+            ads = driver.find_elements(By.ID, "desktop-anchor-close") #find ads
+            if len(ads) > 0:
+                for ad in range(len(ads)):
+                    ads[ad].click()
             #w/r
             WinRate = driver.find_elements(By.CLASS_NAME, "champion-rates") #w/r + games
             if len(WinRate) > 0:
@@ -52,35 +63,44 @@ try:
                 games = int(win)+int(loss)
                 players_stats["WinRate"] = int(win_rate)
                 players_stats["Games"] = games
-            else:
+            else: #player has the weird no-info bug on his stat page (very rare, happened once in many players we checked)
                 driver.back()
                 driver.back()
-                time.sleep(10)
+                time.sleep(10) #wait for page to load. as this happens VERY rarely and in uncertain situations, we will not be using explicit waits here.
                 continue
             #kda
             kda_stats = driver.find_elements(By.CLASS_NAME, "kda")[0].find_elements(By.TAG_NAME, "strong")
-
             players_stats["KDA"] = float(kda_stats[0].text)
             players_stats["AVG_kills"] = float(kda_stats[1].text)
             players_stats["AVG_deaths"] = float(kda_stats[2].text)
             players_stats["AVG_assists"] = float(kda_stats[3].text)
-
             #cs
             players_stats["Creep_Score"] = float(driver.find_elements(By.CLASS_NAME, "cs-cell")[1].text)
 
             #damage
-            dmg = driver.find_elements(By.CLASS_NAME, "damage-cell")[1].text.split(",")
-            players_stats["Damage"] = int(dmg[0] + dmg[1])
-
+            dmg = driver.find_elements(By.CLASS_NAME, "damage-cell")[1].text
+            if "," in dmg:
+                dmg = dmg.split(",")
+                players_stats["Damage"] = int(dmg[0] + dmg[1])
+            else:
+                players_stats["Damage"] = int(dmg) #apparently some ppl have below 1k dmg/gold per game. we won't be using them, but need to filter them to not crash the crawling.
             #gold
-            gold = driver.find_elements(By.CLASS_NAME, "gold-cell")[1].text.split(",")
-            players_stats["Gold"] = int(gold[0]+gold[1])
-
+            gold = driver.find_elements(By.CLASS_NAME, "gold-cell")[1].text
+            if "," in gold:
+                gold = gold.split(",")
+                players_stats["Gold"] = int(gold[0]+gold[1])
+            else:
+                gold = int(gold)
             #multi-kill
             players_stats["Multi_Kill"] = 0
+
             for kill in range(4): #combine the 4 multi kill types into 1 multi-kill
                 if driver.find_elements(By.CLASS_NAME, "multikill-cell")[kill+4].text != "—": #needs to be kill+4
-                    players_stats["Multi_Kill"] += int(driver.find_elements(By.CLASS_NAME, "multikill-cell")[kill+4].text)
+                    multis = driver.find_elements(By.CLASS_NAME, "multikill-cell")[kill+4].text
+                    if len(multis) > 3:
+                        multis = multis.split(",")
+                        multis = int(multis[0]+multis[1])
+                    players_stats["Multi_Kill"] += int(multis)
 
             df = pd.DataFrame([players_stats])
             data += 1
@@ -89,19 +109,21 @@ try:
 
             driver.back()
             driver.back()
-            time.sleep(10)
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "summoner")))
 
         if curr_player >= len(players_list): #check if done with page
-            if page <= 10:
+            if page <= 20:
                 page += 1
+            elif page <= 100:
+                page += 6
             elif page <= 1000:
-                page += 500
+                page += 112
             elif page <= 10000:
-                page += 1000
+                page += 202
             elif page <= 20000:
-                page += 2000
+                page += 452
             else:
-                page += 3000
+                page += 666
 
             now = datetime.now()
             current_time = now.strftime("%H:%M:%S")  # https://www.programiz.com/python-programming/datetime/current-time
@@ -109,7 +131,7 @@ try:
             print(f"going to page {page} at {current_time}, currently have {data*11} data")
             driver.get(url+f"&page={page}")
             curr_player = -1
-            time.sleep(8)
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "summoner")))
 
         stop = driver.find_elements(By.CLASS_NAME, "content-section leaderboard_table_error") #check for no-content error (means we are overflowing players pages, no content left)
         if stop != []:

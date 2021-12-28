4124c9f5cda36eefb905a133ce1d24bf52c784e3
YarinShelek <yarinshelek@gmail.com>
2021-12-28 22:49:32 +0200

    SHAKING MY HEAD OR HOW CAN YOU NOT SEE TGHIS?!

--- crawl.py
+++ crawl.py
@@ -45,7 +45,7 @@ try:
 
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "rank-text"))) #wait for page to load
                 rank = driver.find_element(By.CLASS_NAME, "rank-text").find_element(By.TAG_NAME, "strong").text
-                    if rank == "Unranked":
+                if rank == "Unranked":
                         driver.find_element(By.CLASS_NAME, "flex-center").click() #update the rank
                         time.sleep(10)
                         rank = driver.find_element(By.CLASS_NAME, "rank-text").find_element(By.TAG_NAME, "strong").text  #get his ranking info
